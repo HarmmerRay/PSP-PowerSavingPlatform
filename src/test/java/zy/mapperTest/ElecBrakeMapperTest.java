28d348125b213551a4272105151339d19122a28a
hasee <2624773733@qq.com>
2022-09-25 09:04:10 +0800

    second commit 9.23

--- src/test/java/zy/mapperTest/ElecBrakeMapperTest.java
+++ src/test/java/zy/mapperTest/ElecBrakeMapperTest.java
@@ -4,6 +4,7 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.example.entity.ElecBrake;
 import org.example.mapper.zy.ElecBrakeMapper;
 import org.example.mapper.zy.impl.ElecBrakeMapperImpl;
 import org.junit.Before;
@@ -11,6 +12,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Date;
 
 public class ElecBrakeMapperTest {
     private SqlSession sqlSession;
@@ -26,14 +28,33 @@ public class ElecBrakeMapperTest {
 
     @Test
     public void insert(){
-
+        ElecBrake elecBrake=new ElecBrake(1,"000000",1,220F,22F,4840F,10000F,26.6F,new Date(),new Date());
+        int rv=elecBrakeMapper.insertElecBrake(elecBrake);
+        System.out.println(rv);
     }
     @Test
-    public void delete(){}
+    public void delete(){
+        ElecBrake elecBrake=new ElecBrake(1,"000000",1,220F,22F,4840F,10000F,26.6F,new Date(),new Date());
+        int rv=elecBrakeMapper.deleteElecBrake(elecBrake);
+        System.out.println(rv);
+    }
     @Test
-    public void update(){}
+    public void update(){
+        ElecBrake elecBrake=new ElecBrake(1,"000000",1,220F,22F,4840F,10000F,26.6F,new Date(),new Date());
+        int rv=elecBrakeMapper.updateElecBrake(elecBrake);
+        System.out.println(rv);
+    }
     @Test
-    public void select(){}
+    public void select(){
+        ElecBrake elecBrake=new ElecBrake(1,"000000",1,220F,22F,4840F,10000F,26.6F,new Date(),new Date());
+        ElecBrake elecBrake1=elecBrakeMapper.selectElecBrake(elecBrake);
+        System.out.println(elecBrake1);
+    }
     @Test
-    public void selectAll(){}
+    public void selectAll(){
+        for (ElecBrake e :
+                elecBrakeMapper.selectAll()) {
+            System.out.println(e);
+        }
+    }
 }
--- src/test/java/zy/mapperTest/SocketWeekMapperTest.java
+++ src/test/java/zy/mapperTest/SocketWeekMapperTest.java
@@ -4,6 +4,7 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.example.entity.Socket;
 import org.example.mapper.zy.SocketWeekMapper;
 import org.example.mapper.zy.impl.SocketWeekMapperImpl;
 import org.junit.Before;
@@ -11,6 +12,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Date;
 
 public class SocketWeekMapperTest {
     private SqlSession sqlSession;
@@ -26,14 +28,32 @@ public class SocketWeekMapperTest {
 
     @Test
     public void insert(){
-
+        Socket socket=new Socket(1,1,"000000",1, 220.0F, 20.0F, 4400.0F, 10000F, (float) 27.1,new Date(),new Date());
+        int returnValue=socketWeekMapper.insertSocketWeek(socket);
+        System.out.println(returnValue);
     }
     @Test
-    public void delete(){}
+    public void delete(){
+        Socket socket=new Socket(1,1,"000000",1, 220.0F, 20.0F, 4400.0F, 10000F, (float) 27.1,new Date(),new Date());
+        int rv=socketWeekMapper.deleteSocketWeek(socket);
+        System.out.println(rv);
+    }
     @Test
-    public void update(){}
+    public void update(){
+        Socket socket=new Socket(1,1,"000000",1, 220.0F, 20.0F, 4400.0F, 10000F, (float) 27.1,new Date(),new Date());
+        int rv=socketWeekMapper.updateSocketWeek(socket);
+        System.out.println(rv);
+    }
     @Test
-    public void select(){}
+    public void select(){
+        Socket socket=new Socket(1,1,"000000",1, 220.0F, 20.0F, 4400.0F, 10000F, (float) 27.1,new Date(),new Date());
+        System.out.println(socketWeekMapper.selectSocketWeek(socket));
+    }
     @Test
-    public void selectAll(){}
+    public void selectAll(){
+        for (Socket s:
+                socketWeekMapper.selectAll()) {
+            System.out.println(s);
+        }
+    }
 }
